fffcedd40dfb5c7def82061dd865a87769dfb075
JazJurin <jazjurin@gmail.com>
2023-07-10 18:19:26 +0200

    Ejercicio 7 (falta check)

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,98 @@
-
 // Exercise 6
 function validate() {
-	var error = 0;
+	let error = 0;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	let fName = document.getElementById("fName");
+	let fEmail = document.getElementById("fEmail");
+	let fAddress = document.getElementById("fAddress");
+	let fLastN = document.getElementById("fLastN");
+	let fPassword = document.getElementById("fPassword");
+	let fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	let errorName = document.getElementById("errorName");
+	let errorEmail = document.getElementById("errorEmail");
+	let errorAddress = document.getElementById("errorAddress");
+	let errorLastN = document.getElementById("errorLastN");
+	let errorPassword = document.getElementById("errorPassword");
+	let errorPhone = document.getElementById("errorPhone");
+
+	// Regular Expressions
+	let nameRegEx = /^[a-zA-Z]+$/;
+	let emailRegEx = /\S+@\S+\.\S+/;
+	let passwordRegEx = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+	let phoneRgEx = /^\d+$/;
+
+	// Validate name
+	if (fName.value === "" || fName.value.length < 3 || !nameRegEx.test(fName.value)) {
+		fName.classList.add("is-invalid");
+		errorName.style.display = "";
+		errorName.textContent =
+			"This field is required and must contain at least 3 letters";
 		error++;
+	} else {
+		fName.classList.remove("is-invalid");
 	}
 
-	if(fEmail.value == ""){
+	// Validate email
+	if (fEmail.value === "" || !emailRegEx.test(fEmail.value)) {
+		fEmail.classList.add("is-invalid");
+		errorEmail.style.display = "";
+		errorEmail.textContent =
+			"This field is required and must be a valid email address";
 		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
+	} else {
+		fEmail.classList.remove("is-invalid");
 	}
 
+	// Validate Address
+	if (fAddress.value === "" || fAddress.value.length < 3 || !nameRegEx.test(fAddress.value)) {
+		fAddress.classList.add("is-invalid");
+		errorAddress.style.display = "";
+		errorAddress.textContent =
+			"This field is required and must contain at least 3 characters";
+		error++;
+	} else {
+		fAddress.classList.remove("is-invalid");
+	}
+
+	//Validate Lastname
+	if (
+		fLastN.value === "" || fLastN.value.length < 3 || !nameRegEx.test(fLastN.value)) {
+		fLastN.classList.add("is-invalid");
+		errorLastN.style.display = "";
+		errorLastN.textContent =
+			"This field is required and must contain at least 3 letters";
+		error++;
+	} else {
+		fLastN.classList.remove("is-invalid");
+	}
+
+	// Validate Password 
+	if (fPassword.value === "" || !passwordRegEx.test(fPassword.value)) {
+		fPassword.classList.add("is-invalid");
+		errorPassword.style.display = "";
+		errorPassword.textContent =
+			"This field is required and must contain at least one letter and one number";
+		error++;
+	} else {
+		fPassword.classList.remove("is-invalid");
+	}
+
+	//Validate Phone
+	if (fPhone.value === "" || !phoneRgEx.test(fPhone.value)) {
+		fPhone.classList.add("is-invalid");
+		errorPhone.style.display = "";
+		errorPhone.textContent =
+			"This field is required and must contain only numbers";
+		error++;
+	} else {
+		fPhone.classList.remove("is-invalid");
+	}
+
+	if (error > 0) {
+		alert("Error!")
+	} else {
+		alert("OK!")
+	}
 }
